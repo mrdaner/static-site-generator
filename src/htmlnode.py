a7eed0f04d76628752476e3af1597021b3a14e09
mrdaner <lakhnodima@gmail.com>
2025-03-02 17:49:49 +0000

    D: TestTextNodeToHTMLNode Class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,17 +33,21 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
-    
+
+
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
         if self.tag is None:
-            raise ValueError("invalid HTML: no value")
+            raise ValueError("invalid HTML: no tag")
         if self.children is None:
             raise ValueError("invalid HTML: no children")
-        string_value = ""
+        children_html = ""
         for child in self.children:
-            string_value += child.to_html()
-        return f"<{self.tag}{self.props_to_html()}>{string_value}</{self.tag}>"
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
